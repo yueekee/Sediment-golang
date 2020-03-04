6142cc9b3a05bd290452414a7b683c43d70cb8be
yueekee <yuekewin@gmail.com>
2020-03-04 23:00:55 +0800

    feat: add some demo

---
+++ Interview/04_Map相关/03_map存储.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Test struct {
+	Name string
+}
+
+/*
+map的value本身是不可寻址的，因为map是可以自动扩容的，扩容后的地址会发生变化
+使用list["name"].Name，go程序会报错
+若改为var list map[string]*Test上面的使用就没有问题
+*/
+var list map[string]Test
+
+func main() {
+
+	list = make(map[string]Test)
+	name := Test{"xiaoming"}
+	list["name"] = name
+	list["name"].Name = "Hello"
+	fmt.Println(list["name"])
+}
---
+++ Interview/05_interface相关/03_interface的值和地址.go
@@ -0,0 +1,25 @@
+package main
+
+type S struct {
+}
+
+func f(x interface{}) {
+}
+
+func g(x *interface{}) {
+}
+
+func main() {
+	s := S{}
+	p := &s
+	f(s) //A
+	g(s) //B
+	f(p) //C
+	g(p) //D
+}
+
+/*
+golang是强类型语言（变量定义好类型，类型就不能再改变），interface是所有类型的父类，
+f(x interface{})支持传入所有类型
+而g(x *interface{})只能接收*interface{}
+*/
