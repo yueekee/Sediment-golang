4ed42f51554ec224d1c6d9db524ef11733ba08fb
yueke <yueke@chuanghehui.cn>
2020-05-31 10:52:50 +0800

    ci: slice region

--- Basis/06_切片的拷贝/copy.go
+++ Basis/06_切片的拷贝/copy.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 /* copy函数
-p.s. 使用char1[0:n]也可以实现这种方法
+p.s.
+使用char1[0:n]也可以实现这种方法
+char1[0:n]左闭右开区间
 */
 
 func main() {
 	char1 := []string{"a", "b", "c", "d", "e"}
 	char2 := make([]string, 3)
 	//char2 = char1[:3]
-	copy(char2, char1[:3])
+	copy(char2, char1[1:3])
 
 	fmt.Println("char1:", char1)
 	fmt.Printf("char1:%p\n", char1)
