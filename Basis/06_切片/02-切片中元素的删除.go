65f699501877206e8d6aeb48b1652157fb5b21b5
yueke <yuekewin@gmail.com>
2020-07-15 23:30:56 +0800

    fix: 切片删除指定元素的bug

--- Basis/06_切片/02-切片中元素的删除.go
+++ Basis/06_切片/02-切片中元素的删除.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	sli := []int{0,0,0,1,2,3,4,5,6,7,8,9}
-	n := 0
+	n := 8
 	ints := RemoveParam(sli, n)
 	fmt.Println(ints)
 }
@@ -14,12 +14,12 @@ func RemoveParam(sli []int, n int) []int {
 		if sli[i] == n {
 			if i == 0 {
 				sli = sli[1:]
-				i-- // 如果索引0被去掉后，原来索引1的会移动到索引0
 			} else if i == len(sli) - 1 {
 				sli = sli[:i]
 			} else {
 				sli = append(sli[:i], sli[i+1:]...)
 			}
+			i-- // 如果索引i被去掉后，原来索引i+1的会移动到索引i
 		}
 	}
 	return sli
