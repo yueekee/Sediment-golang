420a6f557fb26fe43a92191f72b3a7e3205d1576
yueke <yuekewin@gmail.com>
2020-07-30 09:28:55 +0800

    ci: url decode

--- Basis/100_other/01_urlencode.go
+++ Basis/100_other/01_urlencode.go
@@ -11,13 +11,16 @@ func main() {
 	v := url.Values{}
 	v.Add("msg", "http://localhost:9999/&a=12")
 	body := v.Encode()
-	fmt.Println("s:", v.Get("msg"))
-	//fmt.Println(v)
-	fmt.Println("body:", body)	// http%3a%2f%2flocalhost%3a9999%2f%26a%3d12
-	fmt.Println("trim:", strings.TrimLeft(body, "msg="))
+	fmt.Println("s:", v.Get("msg")) // msg->http://localhost:9999/&a=12
+	fmt.Println("body:", body)      // body: msg=http%3A%2F%2Flocalhost%3A9999%2F%26a%3D12
+
+	trimLeft := strings.TrimLeft(body, "msg=")
+	fmt.Println("trim:", trimLeft)  // trim: http%3A%2F%2Flocalhost%3A9999%2F%26a%3D12
 
 	// url decode
 	m, _ := url.ParseQuery(body)
-	fmt.Println("m:", m)
-
+	fmt.Println("m:", m["msg"][0])  // m: map[msg:[http://localhost:9999/&a=12]]
+	// url decode 2
+	unescape, _ := url.QueryUnescape(trimLeft)
+	fmt.Println("un:", unescape)
 }
