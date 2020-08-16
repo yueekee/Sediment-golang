516aaa6acc2954d37f81ae7e50213d2d4ac9069f
yueke <yuekewin@gmail.com>
2020-08-16 12:08:23 +0800

    build: Juha

---
+++ Basis/07_判断map中的值的存在/02-for_range.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var m = make(map[string]int)
+	m["1"] = 1
+	m["2"] = 2
+	m["3"] = 3
+
+	for s, i := range m {
+		fmt.Println(s, i)
+	}
+
+	// 切片的forr是从0索引开始的
+	var n = []int{1, 2, 3}
+	for i, i2 := range n {
+		fmt.Println(i, i2)
+	}
+}
