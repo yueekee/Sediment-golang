2914babb4b4ad134b1645e72728a47ebfadfdab8
yueke <yuekewin@gmail.com>
2020-09-24 15:16:25 +0800

    refactor(12/time_sub.go): time

--- Basis/12_time函数/04-time_sub.go
+++ Basis/12_time函数/04-time_sub.go
@@ -5,27 +5,65 @@ import (
 	"time"
 )
 
+/*
+time包有几种格式：
+1.Time
+2.string
+3.Duration 时间间隔 nanosecond 纳秒，格式为24h0m0s
+*/
+
 func main() {
+	// time.Parse 将string->Time，并返回Time
+	startTimeString := "1970-01-01 00:00:00"
+	startTime, _ := time.Parse("2006-01-02 15:04:05", startTimeString)
+	fmt.Println("startTime:", startTime)	// startTime: 1970-01-01 00:00:00 +0000 UTC
 
-	payDate := "2020-06-09 10:51:06"
-	t := time.Now()
-	location, _ := time.ParseInLocation("2006-01-02 15:04:05", payDate, time.Local)
-	t2 := location.AddDate(0, 0, 1)
-	sub := t2.Sub(t)
-	fmt.Println(sub.Seconds())
-	fmt.Println("___________")
+	endTimeString := "2100-06-05 00:00:00"
+	endTime, _ := time.Parse("2006-01-02 15:04:05", endTimeString)
+	fmt.Println("endTime:", endTime)	// endTime: 2100-06-05 00:00:00 +0000 UTC
 
-	// 3天前
-	add := t.Add(-time.Hour * 24 * 3)
-	fmt.Println("++add", add)
-
-	startTime := "1970-01-01 00:00:00"
-	parse, _ := time.Parse("2006-01-02 15:04:05", startTime) // 格式要求比较高
-	fmt.Println("+++parse:", parse)
-	endTime := "2100-06-05 00:00:00"
-	parse2, _ := time.Parse("2006-01-02 15:04:05", endTime)
-	fmt.Println("+++parse2:", parse2)
-	if parse.After(parse2) {
-		fmt.Println("111")
+	// After用于比较2个Time格式的时间点
+	if endTime.After(startTime) {
+		fmt.Println("endTime is after startTime")
 	}
+
+	// time.ParseInLocation 将string转化为当地的Time,并返回Time
+	time1String := "2020-06-09 10:51:06"
+	Time1, _ := time.ParseInLocation("2006-01-02 15:04:05", time1String, time.Local)
+	fmt.Println("Time1:", Time1)	// Time1: 2020-06-09 10:51:06 +0800 CST
+
+	// Time1.AddDate 增加天数
+	Time2 := Time1.AddDate(0, 0, 1)
+	fmt.Println("Time2:", Time2)	// Time2: 2020-06-10 10:51:06 +0800 CST
+
+	// Sub 前面的时间 减 后面的时间
+	DurationSub := Time2.Sub(Time1)
+	fmt.Println("DurationSub:", DurationSub) // 24h0m0s
+	fmt.Println("DurationSub.String:", DurationSub.String()) // 24h0m0s
+
+	// Add, Time 加 时间间隔，返回一个Time
+	Time3 := Time1.Add(DurationSub)
+	fmt.Println("Time3:", Time3)	// Time3: 2020-06-10 10:51:06 +0800 CST 和 Time2时间相同
+
+	// 将时间调至3day前。 Time最大时间尺度是Hour
+	TimeSub3Day := Time1.Add(-time.Hour * 24 * 3)
+	fmt.Println("TimeSub3Day:", TimeSub3Day)	// TimeSub3Day: 2020-06-06 10:51:06 +0800 CST
+
+	// 将时间调至1年前
+	TimeSub1Year := Time1.AddDate(-1, 0, 0)
+	fmt.Println("TimeSub1Year:", TimeSub1Year)	// TimeSub1Year: 2019-06-09 10:51:06 +0800 CST
+
+	// Truncate 返回最接近但早于Time1的时间点;如果DurationSub<0，则但会Time1的copy
+	// Time1: 2020-06-09 10:51:06 +0800 CST
+	DurationTruncate := Time1.Truncate(DurationSub)
+	fmt.Println("DurationTruncate:", DurationTruncate) // DurationTruncate: 2020-06-09 08:00:00 +0800 CST 这里是按天算
+
+	DurationTruncate2 := Time1.Truncate(time.Hour)
+	fmt.Println("DurationTruncate2:", DurationTruncate2)	// DurationTruncate2: 2020-06-09 10:00:00 +0800 CST
+
+	DurationTruncate3 := Time1.Truncate(time.Second)
+	fmt.Println("DurationTruncate3:", DurationTruncate3)	// DurationTruncate3: 2020-06-09 10:51:06 +0800 CST
+
+	DurationTruncate4 := Time1.Truncate(-time.Hour)
+	fmt.Println("DurationTruncate4:", DurationTruncate4)	// DurationTruncate4: 2020-06-09 10:51:06 +0800 CST
 }
