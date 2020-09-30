d8074c5ab17c509748016e73fe71428329305c55
yueke <yuekewin@gmail.com>
2020-09-30 14:05:49 +0800

    ci: leetcode链表1

--- Basis/11_算法/02-leetcode/03-链表/01-删除链表中的节点.go
+++ Basis/11_算法/02-leetcode/03-链表/01-删除链表中的节点.go
@@ -2,6 +2,7 @@ package main
 
 /*
 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点。传入函数的唯一参数为 要被删除的节点 。
+
 提示：
 
 链表至少包含两个节点。
@@ -14,19 +15,13 @@ func main() {
 
 }
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
+// O(1)
 func deleteNode(node *ListNode) {
-
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
 }
