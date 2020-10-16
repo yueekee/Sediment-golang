008e65ac362dd38669cf08c52dc565028b622c8d
yueke <yuekewin@gmail.com>
2020-10-16 14:23:04 +0800

    ci: add time

--- Basis/12-time函数/02-时间戳.go
+++ Basis/12-time函数/02-时间戳.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -14,5 +16,9 @@ func main() {
 	fmt.Println(time.Now().Unix())
 	fmt.Println(time.Now().UnixNano())
 	fmt.Println(time.Now().Local())
+
+	timeStamp := time.Now().Format("2006-01-02 15:04:05")
+	rand.Seed(time.Now().UnixNano())
+	timeStamp = timeStamp + strconv.Itoa(rand.Intn(1000))
 
 }
