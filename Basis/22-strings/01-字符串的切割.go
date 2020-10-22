71ab19360446340d4143db11b8a3d21e5d3f27d7
yueke <yuekewin@gmail.com>
2020-10-22 11:21:29 +0800

    ci: strings

--- Basis/22-strings/01-字符串的切割.go
+++ Basis/22-strings/01-字符串的切割.go
@@ -11,4 +11,15 @@ func main() {
 	fmt.Println("len:", len(trim))
 	fmt.Println("trim", trim)
 	fmt.Println("trim[0]", trim[0])
+
+
+	s2 := "2020102114505434/20201021141122138_1.txt"
+	fmt.Println("s2:", s2[36:])
+
+	s3 := "2020102114505434/1.txt"
+	if len(s3) >= 36 {
+		fmt.Println("s3:", s3[36:])
+	}
+
+
 }
