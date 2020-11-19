ed02bc47ab3a130cad890cd7efec59b86f8682a0
yueke <yuekewin@gmail.com>
2020-11-19 17:55:46 +0800

    test

--- Interview/04_Map相关/01_Map的线程安全.go
+++ Interview/04_Map相关/01_Map的线程安全.go
@@ -1,8 +1,10 @@
 package _4_Map相关
 
 import (
+	"fmt"
 	"strconv"
 	"sync"
+	"time"
 )
 
 /* 注意：
@@ -45,4 +47,31 @@ func main() {
 		}(i)
 	}
 
+}
+
+var lock sync.Mutex
+
+func mapSafe() {
+	m := make(map[int]int)
+	go func() { //开一个协程写map
+		for i := 0; i < 10000; i++ {
+			lock.Lock() //加锁
+			m[i] = i
+			lock.Unlock() //解锁
+		}
+	}()
+	go func() { //开一个协程读map
+		for i := 0; i < 10000; i++ {
+			lock.Lock() //加锁
+			fmt.Println(m[i])
+			lock.Unlock() //解锁
+		}
+	}()
+	time.Sleep(time.Second * 20)
+
+	lock.Lock()
+	for i := 0; i < 10000; i++ {
+		fmt.Println(m[i])
+	}
+	lock.Unlock()
 }
