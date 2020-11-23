1821f19bbd7ad81295ce05bac0da653fac1b4925
yueke <yuekewin@gmail.com>
2020-11-23 22:29:55 +0800

    ci: add deep comparison

---
+++ Basis/100-other/13-deep_comparison.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type data struct {
+	num    int
+	checks [10]func() bool
+	doit   func() bool
+	m      map[string]string
+	bytes  []byte
+}
+
+func main() {
+	v1 := data{}
+	v2 := data{}
+	fmt.Println("v1 == v2:", reflect.DeepEqual(v1, v2))
+	// v1 == v2: true
+
+	m1 := map[int]string{1:"1", 2:"2"}
+	m2 := map[int]string{2:"2", 1:"1"}
+	fmt.Println("m1 == m2:", reflect.DeepEqual(m1, m2))
+	// m1 == m2: true
+
+	s1 := []int{1, 2, 3}
+	s2 := []int{1, 2, 3}
+	fmt.Println("s1 == s2:", reflect.DeepEqual(s1, s2))
+}
