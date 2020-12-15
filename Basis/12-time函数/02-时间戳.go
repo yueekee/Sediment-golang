77324a6dac7f6a8fc93484de9a42b8d6435f7c12
liankui <yuekewin@gmail.com>
2020-12-15 16:15:10 +0800

    style: time

--- Basis/12-time函数/02-时间戳.go
+++ Basis/12-time函数/02-时间戳.go
@@ -8,14 +8,14 @@ import (
 )
 
 func main() {
-	fmt.Println(time.Now())
-	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
-	fmt.Println(time.Now().Second())
-	fmt.Println(time.Now().String())
-	fmt.Println(time.Now().Nanosecond())
-	fmt.Println(time.Now().Unix())
-	fmt.Println(time.Now().UnixNano())
-	fmt.Println(time.Now().Local())
+	fmt.Println(time.Now())                               // 2020-12-15 16:11:18.061577 +0800 CST m=+0.000102257
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05")) // 2020-12-15 16:11:18
+	fmt.Println(time.Now().Second())                      // 18
+	fmt.Println(time.Now().String())                      // 2020-12-15 16:11:18.061745 +0800 CST m=+0.000269899
+	fmt.Println(time.Now().Nanosecond())                  // 61748000
+	fmt.Println(time.Now().Unix())                        // 1608019878
+	fmt.Println(time.Now().UnixNano())                    // 1608019878061751000
+	fmt.Println(time.Now().Local())                       // 2020-12-15 16:11:18.061752 +0800 CST
 
 	timeStamp := time.Now().Format("2006-01-02 15:04:05")
 	rand.Seed(time.Now().UnixNano())
