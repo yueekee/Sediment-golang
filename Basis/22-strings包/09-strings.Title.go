28238c2f22b4d72daefa94d9856162fb6bcc5c62
liankui <yuekewin@gmail.com>
2020-12-28 11:23:14 +0800

    ci: add strings.Title

--- Basis/22-strings包/09-strings.Title.go
+++ Basis/22-strings包/09-strings.Title.go
@@ -7,4 +7,9 @@ import (
 
 func main() {
 	fmt.Println(strings.Title("hello world")) // Hello World 将字符串所包含的每个单词的第一个字母大写
+
+	var width int = 4
+	var length float64 = 7.1
+
+	fmt.Println("area is :", width * int(length))
 }
