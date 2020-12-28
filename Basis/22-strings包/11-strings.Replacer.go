de9f6159b76b5c55820ac9a95d0a36a4e2f36fbb
liankui <yuekewin@gmail.com>
2020-12-28 13:19:49 +0800

    ci: add string.Replacer

--- Basis/22-strings包/11-strings.Replacer.go
+++ Basis/22-strings包/11-strings.Replacer.go
@@ -10,4 +10,8 @@ func main() {
 	replacer := strings.NewReplacer("#1", "o", "s","o")	// 替换是成对出现的。一对中前一个为老字符串，后一个为新字符串
 	fixed := replacer.Replace(broken)
 	fmt.Println(fixed)	// Go rocko
+
+	broken = "G# r#1cks"
+	replace := strings.Replace(broken, "#", "o", 1)
+	fmt.Println(replace)
 }
