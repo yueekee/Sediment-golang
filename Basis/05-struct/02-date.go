8b657e4fcdd9da3abe9ea066572d818bae3516c7
liankui <yuekewin@gmail.com>
2021-01-09 16:17:02 +0800

    build: add Date的封装

---
+++ Basis/05-struct/02-date.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	calendar "github.com/liankui/Sediment-golang/Basis/05-struct/calendar"
+	"log"
+)
+
+// 将程序中的数据隐藏在一部分代码中且对另一部分不可见 的方法成为封装。
+// 它可以用来防止无效数据。也可以修改程序代码的封装部分，不用担心其他代码的访问，而为它们不可直接访问。
+//
+// （这样在包内是可以直接访问未导出数据，但是对包外的，未导出的字段是隐藏的，这时不能意外地将值更改为无效的值）
+func main() {
+	date := calendar.Date{}	// 其中的字段为小写，不能通过赋值的方式输入值了
+	err := date.SetYear(2000) // 只能通过setter方法更新，程序被保护，避免意外输入无效数据。
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetMonth(1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetDay(23)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(date)
+	fmt.Println(date.Year())
+	fmt.Println(date.Month())
+	fmt.Println(date.Day())
+}
---
+++ Basis/05-struct/calendar/data.go
@@ -0,0 +1,44 @@
+package calendar
+
+import "errors"
+
+type Date struct {
+	year  int
+	month int
+	day   int
+}
+
+
+// setter方法是用来将值设置给struct字段或者变量的方法。
+// getter方法是获取struct字段或者变量值的方法。
+func (d *Date) SetYear(year int) error {
+	if year < 1 {
+		return errors.New("invalid year")
+	}
+	d.year = year
+	return nil
+}
+func (d *Date) SetMonth(month int) error {
+	if month < 1 || month > 12 {
+		return errors.New("invalid month")
+	}
+	d.month = month
+	return nil
+}
+func (d *Date) SetDay(day int) error {
+	if day < 1 || day > 31 {
+		return errors.New("invalid day")
+	}
+	d.day = day
+	return nil
+}
+
+func (d *Date) Year() int {
+	return d.year
+}
+func (d *Date) Month() int {
+	return d.month
+}
+func (d *Date) Day() int {
+	return d.day
+}
