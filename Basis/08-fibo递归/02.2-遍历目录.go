a07719ada5dde5fa7ac69617691ed890da1f72bd
liankui <yuekewin@gmail.com>
2021-02-03 13:39:04 +0800

    ci: range files

--- Basis/08-fibo递归/02.2-遍历目录.go
+++ Basis/08-fibo递归/02.2-遍历目录.go
@@ -3,36 +3,28 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"path/filepath"
 )
 
-// filepath.Join()
+// filepath.Join() 用斜杠将目录路径和文件名连接起来
 
-func scanDirectory(path string) error {
+func scanDirectory(path string) {
 	fmt.Println(path)
 	files, err := ioutil.ReadDir(path)	// 获取包含目录内容的切片
 	if err != nil {
-		return err
+		panic(err)
 	}
 
 	for _, file := range files {
-		filePath := filepath.Join(path, file.Name()) // 用斜杠
+		filePath := filepath.Join(path, file.Name()) // 用斜杠将目录路径和文件名连接起来
 		if file.IsDir() {
-			err := scanDirectory(filePath)
-			if err != nil {
-				return err
-			}
+			scanDirectory(filePath)
 		} else {
 			fmt.Println(filePath)
 		}
 	}
-	return nil
 }
 
 func main() {
-	err := scanDirectory("./../05-struct")
-	if err != nil {
-		log.Fatal(err)
-	}
+	scanDirectory("./05-struct")
 }
