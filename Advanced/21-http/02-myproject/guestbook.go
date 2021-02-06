38719f52802b017fc897102b73d7ebc2af25b4a1
liankui <yuekewin@gmail.com>
2021-02-06 22:25:02 +0800

    ci(guestbook): add varb into template, add new.html

--- Advanced/21-http/02-myproject/guestbook.go
+++ Advanced/21-http/02-myproject/guestbook.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func check(err error) {
@@ -12,17 +14,43 @@ func check(err error) {
 	}
 }
 
+type Guestbook struct {
+	SignatureCount int
+	Signatures []string
+}
+
 // http.ResponseWriter 用于更新将发送到浏览器的响应的值
 // *http.Request 表示来自浏览器的请求的值
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getString("signatures.txt")
 	html, err := template.ParseFiles("view.html")	// 使用view.html创建一个新模版
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures: signatures,
+	}
 	check(err)
-	err = html.Execute(writer, nil)	// 将新模版写入ResponseWriter
+	err = html.Execute(writer, guestbook)	// 将新模版写入ResponseWriter
 	check(err)
 	//
 	//message := []byte("Hello,web")
 	//_, err = writer.Write(message) // 将该数据添加到发送到浏览器的响应中
 	//check(err)
+}
+
+func getString(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {	// 如果返回一个错误说文件不存在
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err()) // 报告任何扫描错误
+	return lines
 }
 
 func main() {
