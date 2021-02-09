5d2298411b14d7dfa3b688d4d13cfe4eecd6b264
liankui <yuekewin@gmail.com>
2021-02-09 21:34:34 +0800

    ci: 按位与

--- Basis/12-运算符/01-按位与.go
+++ Basis/12-运算符/01-按位与.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("false && false == %t\n", false && false)
-	fmt.Printf("true && false == %t\n", true && false)
-	fmt.Printf("true && true == %t\n", true && true)
+	// 与
+	fmt.Printf("false && false == %t\n", false && false)	// false && false == false
+	fmt.Printf("true && false == %t\n", true && false)	// true && false == false
+	fmt.Printf("true && true == %t\n", true && true)		// true && true == true
 
+	// 按位与
 	fmt.Printf("%b & %b == %b\n", 0, 0, 0&0) // 0 & 0 == 0
 	fmt.Printf("%b & %b == %b\n", 0, 1, 0&1) // 0 & 1 == 0
 	fmt.Printf("%b & %b == %b\n", 1, 1, 1&1) // 1 & 1 == 1
