f476abf3a6320071303eb3bd3ecfcc07f0087f4f
liankui <yuekewin@gmail.com>
2021-02-19 15:47:17 +0800

    ci: slice range

--- Basis/06-slice/03-数组越界.go
+++ Basis/06-slice/03-数组越界.go
@@ -2,14 +2,23 @@ package main
 
 import "fmt"
 
+// 使用range可以安全的遍历切片，不用担心数组越界的情况
+
 func main() {
 	sli := []int{0, 1}
-	sli2 := []int{}
 	for i, v := range sli {
-		fmt.Printf("sli[%v]=%v\n", i, v)
+		fmt.Printf("sli[%v]=%v\n", i, v) // 正常
+	}
+
+	fmt.Println("-------------")
+
+	sli2 := []int{}
+	for i, v := range sli2 {
+		fmt.Printf("sli[%v]=%v\n", i, v) // 正常
 	}
 	fmt.Println("-------------")
-	for i, v := range sli2 {
-		fmt.Printf("sli[%v]=%v\n", i, v)
+
+	for i := 0; i <= len(sli2); i++ {
+		fmt.Printf("sli2[%v]=%v\n", i, sli2[i]) // panic: runtime error: index out of range [0] with length 0
 	}
 }
