c88c1e2848e5747ca93ceec941a98cf86b1051a5
liankui <yuekewin@gmail.com>
2021-02-20 11:42:05 +0800

    ci: 函数也是引用类型

--- Advanced/21-HTTP/01-一级函数/03-sayHi.go
+++ Advanced/21-HTTP/01-一级函数/03-sayHi.go
@@ -4,6 +4,7 @@ import "fmt"
 
 // 具有一级函数的语言中，可以将函数赋值给变量，然后使用这些变量来调用函数。
 // 当调用其它函数时，函数也可以作为参数传递。
+// 函数也是引用类型。
 
 func sayHi() {
 	fmt.Println("Hi")
@@ -12,5 +13,7 @@ func sayHi() {
 func main() {
 	var myFunction func() // 声明一个类型为func()的变量，该变量可以保存一个函数
 	myFunction = sayHi    // 将sayHi函数赋值给变量
+	fmt.Printf("%#v\n", sayHi)	// (func())(0x10a89c0)
+	fmt.Printf("%p\n", sayHi)	// 0x10a89c0
 	myFunction()          // 调用存储在变量中函数
 }
