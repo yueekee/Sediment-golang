05d4313dbc50ad197dac3c0552db4eef0b507755
liankui <yuekewin@gmail.com>
2021-03-08 22:18:02 +0800

    ci: add slice1

--- Basis/01-指针类型/02-切片复制到指针切片的问题.go
+++ Basis/01-指针类型/02-切片复制到指针切片的问题.go
@@ -19,11 +19,12 @@ func main() {
 }
 
 func test1() {
-	in := []int{1, 2, 3}
+	a, b, c := 1,2,3
+	in := []*int{&a, &b, &c}
 
 	var out []*int
 	for _, v := range in {
-		out = append(out, &v)
+		out = append(out, v)
 	}
 
 	fmt.Println("*out:", *out[0], *out[1], *out[2]) // *out: 3 3 3
