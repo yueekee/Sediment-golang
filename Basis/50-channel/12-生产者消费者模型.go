65eb13066c4a214d903b041579f708a11a56bb24
liankui <yuekewin@gmail.com>
2021-03-09 11:01:02 +0800

    ci: channel

--- Basis/50-channel/12-生产者消费者模型.go
+++ Basis/50-channel/12-生产者消费者模型.go
@@ -2,33 +2,54 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
+// for range ch时要用close
+
 func main() {
-	rand.Seed(time.Now().Unix())
-	ch := make(chan int)
-	// 起5个go程写数据，用主go程读数据
-	for i := 0; i < 5; i++ {
-		go func() {
-			ch <- rand.Intn(100)
-		}()
-		fmt.Println(<-ch)
-	}
+	f1()
+	f2()
+	f3()
 }
 
 // 写法1
 func f1() {
-	rand.Seed(time.Now().Unix())
 	ch := make(chan int)
 	// 起一个go程，写入5个数据
 	go func() {
 		for i := 0; i < 5; i++ {
-			ch <- rand.Intn(10)
+			ch <- i
 		}
 	}()
 	for i := 0; i < 5; i++ {
 		fmt.Println(<-ch)
 	}
 }
+
+func f2() {
+	var ch = make(chan int)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			ch <-i
+		}
+		close(ch)
+	}()
+
+	for i := range ch {
+		fmt.Println("-------: ", i)
+	}
+}
+
+func f3() {
+	ch := make(chan int)
+
+	// 起5个go程写数据，用主go程读数据
+	for i := 0; i < 5; i++ {
+		go func() {
+			ch <- i
+		}()
+		fmt.Println(<-ch)
+	}
+}
+
