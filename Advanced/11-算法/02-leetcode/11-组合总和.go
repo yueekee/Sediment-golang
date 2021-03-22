a2550b468770c5b318245ef6346bd512836b3679
liankui <yuekewin@gmail.com>
2021-03-22 18:11:44 +0800

    ci: add leetcode

---
+++ Advanced/11-算法/02-leetcode/11-组合总和.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func combSum(candidates []int, target int) (ans [][]int) {
+	comb := []int{}
+	var dfs func(target, idx int)
+	dfs = func(target, idx int) {
+		if idx == len(candidates) {
+			return
+		}
+		if target == 0 {
+			ans = append(ans, append([]int(nil), comb...))
+			return
+		}
+		// 直接跳过
+		dfs(target, idx+1)
+		// 选择当前数
+		if target-candidates[idx] >= 0 {
+			comb = append(comb, candidates[idx])
+			dfs(target-candidates[idx], idx)
+			comb = comb[:len(comb)-1]
+		}
+	}
+	dfs(target, 0)
+	return
+}
+
+func main() {
+	candidates := []int{2,3,6,7}
+	target := 7
+	fmt.Println(combSum(candidates, target))
+}
