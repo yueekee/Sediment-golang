047f1955412af76fdb5c8683d13f1bdd3f048a27
liankui <yuekewin@gmail.com>
2021-03-24 11:16:52 +0800

    build: add iota_string

---
+++ Interview/01-go执行/07-iota_string.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// 注意South为Direction类型
+
+type Direction int
+
+const (
+	North Direction = iota
+	East
+	South
+	West
+)
+
+func (d Direction) String() string {
+	return [...]string{"North", "East", "South", "West"}[d]
+}
+
+func main() {
+	fmt.Println(South)	// South
+}
