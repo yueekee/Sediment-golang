5de0316ef09dcb49e78367f1eb821bf263d74b3e
liankui <yuekewin@gmail.com>
2021-03-26 10:26:12 +0800

    style: map name

--- Interview/11-other/01-string循环输出.go
+++ Interview/11-other/01-string循环输出.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-/*
+/*总结：todo 没看懂
 如果类型实现 String() 方法，当格式化输出时会自动使用 String() 方法。上面这段代码是在该类型的 String() 方法内使用格式化输出，导致递归调用，最后抛错。
 */
 
--- test.go
+++ test.go
@@ -2,16 +2,10 @@ package main
 
 import "fmt"
 
-type ConfigOne struct {
-	Daemon string
-}
-
-func (c *ConfigOne) String() string {
-	fmt.Printf("%#v", c)
-	return fmt.Sprintf("print: %v", c)
-}
-
 func main() {
-	c := &ConfigOne{}
-	c.String()
+	x := interface{}(nil)
+	t, c := x.(interface{})
+	fmt.Printf("%#v\n", x)
+	fmt.Printf("%#v\n", t)
+	fmt.Printf("%#v\n", c)
 }
