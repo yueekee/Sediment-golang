59655a6bea54751a2d183f9a93e32069d92b42f7
liankui <yuekewin@gmail.com>
2021-03-30 21:57:27 +0800

    refactor(switch): add new example

--- Basis/02-switch_goto_闭包/01-switch的2种用法.go
+++ Basis/02-switch_goto_闭包/01-switch的2种用法.go
@@ -22,46 +22,24 @@ switch有2中用法：
 		}
 */
 func main() {
-	var money int
-	switch1(money)
-	//switch2(money)
-}
+	i := 2
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
 
-func switch1(money int) {
-	for {
-		fmt.Print("输入金额:")
-		fmt.Scan(&money)	// 获取键盘输入的值
-		switch {
-		case money >= 4000:
-			fmt.Println("钻石用户")
-		case money >= 3000:
-			fmt.Println("白金用户")
-		case money >= 2000:
-			fmt.Println("黄金用户")
-		case money >= 1000:
-			fmt.Println("白银用户")
-		case money > 0:
-			fmt.Println("普通用户")
-		default:
-			fmt.Println("请输入正确的金额")
-		}
+	fmt.Println("---------")
+	switch {
+	case i == 1:
+		fmt.Println("one")
+	case i == 2:
+		fmt.Println("two")
+	case i == 3:
+		fmt.Println("three")
 	}
 }
 
-func switch2(money int) {
-	for {
-		fmt.Println("输入金额:")
-		fmt.Scan(&money)
-		if money >= 4000 {
-			fmt.Println("钻石用户")
-		} else if money >= 3000 {
-			fmt.Println("白金用户")
-		} else if money >= 2000 {
-			fmt.Println("黄金用户")
-		} else if money >= 1000 {
-			fmt.Println("白银用户")
-		} else {
-			fmt.Println("普通用户")
-		}
-	}
-}
