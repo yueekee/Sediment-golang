d72d8e381a29d0a1d2e9cd82346c183cddc7c30f
liankui <yuekewin@gmail.com>
2021-04-06 20:29:41 +0800

    ci: update flag

--- Basis/23-flag/01-demo.go
+++ Basis/23-flag/01-demo.go
@@ -5,22 +5,19 @@ import (
 	"fmt"
 )
 
-// 定义命令行参数对应的变量，这三个变量都是指针类型
+/*flag总结
+实现命令行参数的解析
+*/
+
+// 定义命令行参数对应的变量，这两个变量都是指针类型
 var cliName = flag.String("name", "nick", "Input Your Name")
 var cliAge = flag.Int("age", 28, "Input Your Age")
-var cliGender = flag.String("gender", "male", "Input Your Gender")
 
-// 定义一个值类型的命令行参数变量，在 Init() 函数中对其初始化
-// 因此，命令行参数对应变量的定义和初始化是可以分开的
-var cliFlag int
-
-func Init() {
-	flag.IntVar(&cliFlag, "flagname", 1234, "Just for demo")
-}
+var gender string
 
 func main() {
-	// 初始化变量 cliFlag
-	Init()
+	// 初始化变量 gender
+	flag.StringVar(&gender, "gender", "male", "put your gender")
 	// 把用户传递的命令行参数解析为对应变量的值
 	flag.Parse()
 
@@ -34,6 +31,14 @@ func main() {
 	// 输出命令行参数
 	fmt.Println("name=", *cliName)
 	fmt.Println("age=", *cliAge)
-	fmt.Println("gender=", *cliGender)
-	fmt.Println("flagname=", cliFlag)
+	fmt.Println("gender=", gender)
 }
+
+/*
+使用命令：go run 01-demo.go -name=eric -age=18 -gender=female help
+args=[help], num=1
+arg[0]=help
+name= eric
+age= 18
+gender= female
+*/
