cd3d5209ad0c617c106cacfcebeac66943e1db28
liankui <yuekewin@gmail.com>
2021-04-12 11:04:58 +0800

    ci: 插入排序法

--- Advanced/11-算法/01-排序算法/02_插入排序法.go
+++ Advanced/11-算法/01-排序算法/02_插入排序法.go
@@ -2,32 +2,23 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 // 将第i位的数字和前面i-1位队列中的数字比较，如果队列中的数对第i位数大，将队列中的数后移。
 
-func insertSort(arr []int) {
-	var i,j int
-	for i = 1; i < len(arr); i++ {
-		temp := arr[i]
-		for j = i; j > 0 && arr[j-1] > temp; j-- {
-			arr[j] = arr[j-1]
+func insertSort(sli []int) {
+	var i, j int
+	for i = 1; i < len(sli); i++ {
+		tmp := sli[i]
+		for j = i; j > 0 && sli[j-1] > tmp; j-- {
+			sli[j] = sli[j-1]
 		}
-		arr[j] = temp
+		sli[j] = tmp
 	}
 }
 
 func main() {
-	startTime := time.Now().UnixNano()
-	for i := 0; i < 100000; i++ {
-		arr := []int{12, 8, 38, 65, 2, 93}
-		insertSort(arr)
-	}
-	endTime := time.Now().UnixNano()
-
-	nanoSeconds:= float64(endTime - startTime)
-
-	//fmt.Println(arr)
-	fmt.Printf("second:%f", nanoSeconds/100000)
+	sli := []int{12, 8, 38, 65, 2, 93}
+	insertSort(sli)
+	fmt.Println(sli)	// [93 65 38 12 8 2]
 }
