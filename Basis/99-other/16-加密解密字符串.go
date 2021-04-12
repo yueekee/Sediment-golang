068e12958005d46ffa1aabe5dfe5b5e538678ea0
liankui <yuekewin@gmail.com>
2021-04-12 17:03:18 +0800

    ci: add ctypt 加密解密字符串操作

---
+++ Basis/99-other/16-加密解密字符串.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"crypto/des"
+	"encoding/hex"
+	"errors"
+	"fmt"
+)
+
+var cryptStr = "FU2fTuYJ"
+
+func Encrypt(text string) (string, error) {
+	key := []byte(cryptStr)
+	src := []byte(text)
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	bs := block.BlockSize()
+	src = ZeroPadding(src, bs)
+	if len(src)%bs != 0 {
+		return "", errors.New("Need a multiple of the block size. ")
+	}
+	out := make([]byte, len(src))
+	dst := out
+	for len(src) > 0 {
+		block.Encrypt(dst, src[:bs])
+		src = src[bs:]
+		dst = dst[bs:]
+	}
+	return hex.EncodeToString(out), nil
+}
+
+func Decrypt(decrypted string) (string, error) {
+	key := []byte(cryptStr)
+	src, err := hex.DecodeString(decrypted)
+	if err != nil {
+		return "", err
+	}
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	out := make([]byte, len(src))
+	dst := out
+	bs := block.BlockSize()
+	if len(src)%bs != 0 {
+		return "", errors.New("crypto/cipher: input not full blocks")
+	}
+	for len(src) > 0 {
+		block.Decrypt(dst, src[:bs])
+		src = src[bs:]
+		dst = dst[bs:]
+	}
+	out = ZeroUnPadding(out)
+	return string(out), nil
+}
+
+func ZeroPadding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{0}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func ZeroUnPadding(origData []byte) []byte {
+	return bytes.TrimFunc(origData,
+		func(r rune) bool {
+			return r == rune(0)
+		})
+}
+
+func main() {
+	encrypt, _ := Encrypt("hello world")
+	decrypt, _ := Decrypt(encrypt)
+	fmt.Println(encrypt, "-", decrypt)	// 1b66e140211590d53cd66e5c2868b2d1 - hello world
+}
