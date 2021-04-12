ff44878dd2c9e2a4b6ea754f05f8649666bd8be0
liankui <yuekewin@gmail.com>
2021-04-12 10:37:22 +0800

    fix: 冒泡排序

--- Advanced/11-算法/01-排序算法/04_冒泡排序法.go
+++ Advanced/11-算法/01-排序算法/04_冒泡排序法.go
@@ -1,30 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func bubbleSort(arr []int) {
-	for i := 1; i < len(arr) - 1; i++ {
-		for j := 0; j < len(arr) - i; j++ {
-			if arr[j+1] > arr[j] {
-				arr[j+1], arr[j] = arr[j], arr[j+1]
+/*冒泡算法
+比较两个相邻的数字的大小，如果左侧的数大则进行交换，直至将大数移到右侧。
+*/
+
+func bubbleSort(sli []int) {
+	for i := 0; i < len(sli); i++ {
+		for j := 1; j < len(sli) - i; j++ {
+			if sli[j] < sli[j-1] {
+				sli[j], sli[j-1] = sli[j-1], sli[j]
 			}
+			//fmt.Printf("i:%d,j:%d,sli:%v\n", i, j, sli)
 		}
 	}
 }
 
 func main() {
-	startTime := time.Now().UnixNano()
-	for i := 0; i < 100000; i++ {
-		arr := []int{12, 8, 38, 65, 2, 93}
-		bubbleSort(arr)
-	}
-	endTime := time.Now().UnixNano()
+	//startTime := time.Now().UnixNano()
+	//for i := 0; i < 100000; i++ {
+	//	arr := []int{12, 8, 38, 65, 2, 93}
+	//	bubbleSort(arr)
+	//}
+	//endTime := time.Now().UnixNano()
+	//
+	//nanoSeconds:= float64(endTime - startTime)
+	//
+	////fmt.Println(arr)
+	//fmt.Printf("second:%f", nanoSeconds/100000)
 
-	nanoSeconds:= float64(endTime - startTime)
-
-	//fmt.Println(arr)
-	fmt.Printf("second:%f", nanoSeconds/100000)
+	sli := []int{12, 8, 38, 65, 2, 93, 1}
+	bubbleSort(sli)
+	fmt.Println(sli)	// [1 2 8 12 38 65 93]
 }
