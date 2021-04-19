9fe25b7846587730c6142297fd7044fae13f718d
liankui <yuekewin@gmail.com>
2021-04-19 22:01:47 +0800

    ci: make and new

--- Basis/07-map/04-make.go
+++ Basis/07-map/04-make.go
@@ -9,10 +9,17 @@ new 创建指定类型的指针空间
 
 func main() {
 	m := make(map[int]string)
-	m[1] = "1"
-	fmt.Println(m)	// map[1:1]
+	//m[1] = "1"
+	fmt.Printf("%#v\n", m)	// map[int]string{}
 
 	var n *map[int]string
 	n = new(map[int]string)
-	fmt.Println(n)	// &map[]
+	fmt.Printf("%#v\n", n)	// &map[int]string(nil)
+
+	s := make([]int, 0)
+	fmt.Printf("%#v\n", s)	// []int{}
+
+	var s2 *[]int
+	s2 = new([]int)
+	fmt.Printf("%#v\n", s2)	// &[]int(nil)
 }
