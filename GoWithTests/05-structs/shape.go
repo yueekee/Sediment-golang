d7cae2af95336f9a805a16f82e95dbc2e037be85
liankui <yuekewin@gmail.com>
2021-04-28 10:30:40 +0800

    build: add structs Test

---
+++ GoWithTests/05-structs/shape.go
@@ -0,0 +1,28 @@
+package structs
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Height float64
+	Width float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return (rectangle.Height + rectangle.Width) * 2
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
+}
