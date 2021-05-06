8e7f4643e679f189d5e36f0b7b4582630a79ee68
liankui <yuekewin@gmail.com>
2021-05-06 17:38:13 +0800

    ci: concurrency 添加多线程版

--- GoWithTests/10-concurrency/check_websites_benchmark_test.go
+++ GoWithTests/10-concurrency/check_websites_benchmark_test.go
@@ -20,3 +20,23 @@ func BenchmarkCheckWebsite(b *testing.B) {
 		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 }
+
+/* 
+单线程版基准测试时间为2.23s
+go test -bench=.
+goos: darwin
+goarch: amd64
+pkg: github.com/liankui/Sediment-golang/GoWithTests/10-concurrency
+BenchmarkCheckWebsite-16               1        2236162528 ns/op
+PASS
+ok      github.com/liankui/Sediment-golang/GoWithTests/10-concurrency   3.473s
+
+并发版的基准测试时间为0.023s，快了100倍
+ go test -bench=.                    
+goos: darwin
+goarch: amd64
+pkg: github.com/liankui/Sediment-golang/GoWithTests/10-concurrency
+BenchmarkCheckWebsite-16              56          23228837 ns/op
+PASS
+ok      github.com/liankui/Sediment-golang/GoWithTests/10-concurrency   2.265s
+ */
