30b87f764e58a9ccfc2d2ff172f3219a366f404f
liankui <yuekewin@gmail.com>
2021-05-08 16:29:10 +0800

    ci: reflection udpate

--- GoWithTests/12-reflection/reflection.go
+++ GoWithTests/12-reflection/reflection.go
@@ -1,1 +1,144 @@
 package main
+
+import (
+	"reflect"
+)
+
+// 第四版
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+	
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			// walk(val.Field(i).Interface(), fn)
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			// walk(val.Index(i).Interface(), fn)
+			walkValue(val.Index(i))
+		}
+
+	case reflect.Map :
+		for _, key := range val.MapKeys() {
+			// walk(val.MapIndex(key).Interface(), fn)
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walk(v.Interface(), fn)
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walk(res.Interface(), fn)
+		}
+	}
+}
+
+// 版本三
+/* func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	numberOfValues := 0
+	var getField func(int) reflect.Value
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice, reflect.Array:
+		numberOfValues = val.Len()
+		getField = val.Index
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
+		}
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
+	}
+} */
+
+// 第二版
+/* func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+	}
+} */
+
+// 第一版
+/* func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+	// fmt.Println("val", val)	// val {Chris London}
+
+	// 对切片进行处理
+	if val.Kind() == reflect.Slice {
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+		return
+	}
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		switch field.Kind(){
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
+		// fmt.Println("field", field)	// field Chris
+	}
+} */
+
+// 对指针类型进行取值处理
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
+}
+
+/* 匿名结构体
+cases := []struct{
+	Name string
+	Input interface{}
+	ExpectedCalls []string
+} {
+	{
+		"Struct with one string field",
+		struct {
+			Name string
+			// City string
+			Age int
+		}{"Chris", 33},
+		[]string{"Chris", "London"},
+	},
+}
+*/
