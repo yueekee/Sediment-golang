e4463f075109acb14a952ca32f32544bae2d3220
liankui <yuekewin@gmail.com>
2021-05-08 14:24:33 +0800

    build: reflection test

---
+++ GoWithTests/12-reflection/reflection_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	expected := "Chris"
+	var got []string
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	}
+
+	if got[0] != expected {
+		t.Errorf("got '%s' want '%s'", got, expected)
+	}
+}
+
+func walk(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	fn(field.String())
+}
