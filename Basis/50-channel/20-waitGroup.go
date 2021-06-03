a0c537fa21920fe9be845b2afff2ce912ce696db
liankui <yuekewin@gmail.com>
2021-06-03 12:31:14 +0800

    ci: add channel

--- Basis/50-channel/20-waitGroup.go
+++ Basis/50-channel/20-waitGroup.go
@@ -12,7 +12,7 @@ import (
 4.使用wait等待任务数为0时结束
 */
 
-func main() {
+func main1() {
 	waitGroup := sync.WaitGroup{}
 	waitGroup.Add(9)
 	for i := 1; i < 10; i++ {
@@ -25,3 +25,19 @@ func main() {
 }
 
 // 输出为乱序的1-9位数
+
+func main() {
+	ch := make(chan int)
+	for i := 1; i < 10; i++ {
+		go func(i int) {
+			ch <- i
+		}(i)
+		defer close(ch)
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-ch)
+	}
+}
+
+// 打印9个数字后，报错fatal error: all goroutines are asleep - deadlock!
