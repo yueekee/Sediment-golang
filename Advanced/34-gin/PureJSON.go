e417dcacb500aff8555c1612d782a7503ddc760b
liankui <yuekewin@gmail.com>
2021-06-07 18:07:22 +0800

    ci: add gin SecureJSON

--- Advanced/34-gin/PureJSON.go
+++ Advanced/34-gin/PureJSON.go
@@ -2,6 +2,8 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+// 通常，JSON 使用 unicode 替换特殊 HTML 字符，例如 < 变为 \ u003c。如果要按字面对这些字符进行编码，则可以使用 PureJSON。
+// Go 1.6 及更低版本无法使用此功能。
 func main() {
 	r := gin.Default()
 
