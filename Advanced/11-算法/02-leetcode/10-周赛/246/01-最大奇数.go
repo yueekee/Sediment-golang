45fc30995b430b2d6720d8c925aa8a7c825af401
liankui <yuekewin@gmail.com>
2021-06-20 11:13:11 +0800

    build: add leetcode 246weekly code

---
+++ Advanced/11-算法/02-leetcode/10-周赛/246/01-最大奇数.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	//num := "52"
+	//num := "10133890"
+	num := "23953767242388496965328710"
+	number := largestOddNumber(num)
+	fmt.Println("----:", number)
+}
+
+func largestOddNumber(num string) string {
+	for i := len(num)-1; i >= 0 ; i-- {
+		atoi, err := strconv.Atoi(string(num[i]))
+		if err != nil {
+			return ""
+		}
+		fmt.Println("atoi:", atoi)
+
+		if atoi%2 != 0 {
+			return num[:i+1]
+		}
+	}
+	return ""
+}
+
